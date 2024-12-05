da71996127ff4dd613c779829b45d73e21fbb427
vilivonka <gromootvod@lenta.ru>
2024-12-05 11:31:49 +0300

    main_mobail4

---
+++ js/main.js
@@ -0,0 +1,42 @@
+(() => {
+
+  let one = document.querySelector('.addition i.one');
+  let too = document.querySelector('.addition i.too');
+  let free = document.querySelector('.addition i.free');
+  let foo = document.querySelector('.addition i.foo');
+  let explanations = document.querySelectorAll('.addition i');
+  let boxText = document.querySelector('.explanation_box');
+  let box = document.querySelector('.explanation');
+
+
+  for (let i = 0; i < explanations.length; i++) {
+    explanations[i].addEventListener('click', function () {
+      boxText.innerHTML = this.getAttribute('data-text');
+      box.classList.add('active');
+      box.style.top = this.getBoundingClientRect().top + window.pageYOffset + 'px';
+      box.style.left = this.getBoundingClientRect().left + 'px';
+      if(window.innerWidth < 500 ){
+        box.style.left = '20px';
+      }
+    })
+  }
+
+
+  document.addEventListener('click', (e) => {
+    const lkEl = e.composedPath().includes(box);
+    const gambLk1 = e.composedPath().includes(one);
+    const gambLk2 = e.composedPath().includes(too);
+    const gambLk3 = e.composedPath().includes(free);
+    const gambLk4 = e.composedPath().includes(foo);
+    if (!lkEl && !gambLk1) {
+      if (!lkEl && !gambLk2) {
+        if (!lkEl && !gambLk3) {
+          if (!lkEl && !gambLk4) {
+            box.classList.remove('active');
+          }
+        }
+      }
+    }
+  })
+
+})();
