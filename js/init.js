8b51df6f83168076483438a842f57cc4cacfbb48
vilivonka <gromootvod@lenta.ru>
2024-12-11 17:08:55 +0300

    standart_scroll_no

--- js/init.js
+++ js/init.js
@@ -223,6 +223,40 @@ $(function () {
   var accordion = new Accordion($('.js_catalog_box_filter7'), false);
 });
 
+$(function () {
+  var Accordion = function (el, multiple) {
+    this.el = el || {};
+    // more then one submenu open?
+    this.multiple = multiple || false;
+
+    var accordionHeader = this.el.find('.catalog_box_filter_head');
+    accordionHeader.on('click', {
+      el: this.el,
+      multiple: this.multiple
+    },
+      this.dropdown);
+  };
+
+  Accordion.prototype.dropdown = function (e) {
+    var $el = e.data.el,
+      $this = $(this),
+      //this is the ul.submenuItems
+      $next = $this.next();
+
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+
+    if (!e.data.multiple) {
+      //show only one menu at the same time
+      $el.find('.catalog_box_filter_body').not($next).slideUp().parent().removeClass('open');
+    }
+  }
+
+  var accordion = new Accordion($('.js_catalog_box_filter8'), false);
+});
+
+
+
 
 
 $(function () {
@@ -449,3 +483,36 @@ $(function () {
 
   var accordion = new Accordion($('.js_catalog_box_filter17'), false);
 });
+
+$(function () {
+  var Accordion = function (el, multiple) {
+    this.el = el || {};
+    // more then one submenu open?
+    this.multiple = multiple || false;
+
+    var accordionHeader = this.el.find('.catalog_box_filter_head');
+    accordionHeader.on('click', {
+      el: this.el,
+      multiple: this.multiple
+    },
+      this.dropdown);
+  };
+
+  Accordion.prototype.dropdown = function (e) {
+    var $el = e.data.el,
+      $this = $(this),
+      //this is the ul.submenuItems
+      $next = $this.next();
+
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+
+    if (!e.data.multiple) {
+      //show only one menu at the same time
+      $el.find('.catalog_box_filter_body').not($next).slideUp().parent().removeClass('open');
+    }
+  }
+
+  var accordion = new Accordion($('.js_catalog_box_filter18'), false);
+});
+
