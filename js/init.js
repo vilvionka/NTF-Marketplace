49e92db4acd71ed76cd17e49c4441c2b40ef2790
vilivonka <gromootvod@lenta.ru>
2024-12-20 21:57:05 +0300

    form3

--- js/init.js
+++ js/init.js
@@ -559,3 +559,27 @@ $(function () {
   var accordion = new Accordion($('.js_catalog_box_filter18'), false);
 });
 
+$(function() {
+ 
+  (function quantityProducts() {
+    var $quantityArrowMinus = $(".quantity-arrow-minus");
+    var $quantityArrowPlus = $(".quantity-arrow-plus");
+    var $quantityNum = $(".quantity-num");
+ 
+    $quantityArrowMinus.click(quantityMinus);
+    $quantityArrowPlus.click(quantityPlus);
+ 
+    function quantityMinus(e) {
+      e.preventDefault();
+      if ($quantityNum.val() > 1) {
+        $quantityNum.val(+$quantityNum.val() - 1);
+      }
+    }
+ 
+    function quantityPlus(e) {
+      e.preventDefault();
+      $quantityNum.val(+$quantityNum.val() + 1);
+    }
+  })();
+ 
+});
