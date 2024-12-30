384cb4edebf6de8ea1729d0db75a40a7d2b639da
vilivonka <gromootvod@lenta.ru>
2024-12-30 17:06:12 +0300

    last sdacha

--- js/init.js
+++ js/init.js
@@ -574,3 +574,36 @@ $(function() {
   })();
  
 });
+
+
+$(function () {
+  var Accordion = function (el, multiple) {
+    this.el = el || {};
+    // more then one submenu open?
+    this.multiple = multiple || false;
+
+    var accordionHeader = this.el.find('.menu_pop_accordion_header');
+    accordionHeader.on('click', {
+      el: this.el,
+      multiple: this.multiple
+    },
+      this.dropdown);
+  };
+
+  Accordion.prototype.dropdown = function (e) {
+    var $el = e.data.el,
+      $this = $(this),
+      //this is the ul.submenuItems
+      $next = $this.next();
+
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+
+    if (!e.data.multiple) {
+      //show only one menu at the same time
+      $el.find('.menu_pop_accordion_body').not($next).slideUp().parent().removeClass('open');
+    }
+  }
+
+  var accordion = new Accordion($('.menu_pop_accordion'), false);
+});
